6dc7e2a1e2e11af3cbf3045768a5af063ed1a255
AndreasLouw <andreaslouw@gmail.com>
2018-05-09 11:33:49 +0200

    Db added

---
+++ dbinf354.sql
@@ -0,0 +1,44 @@
+Use Master
+Go
+If Exists (Select * from sys.databases where name = 'INF354')
+DROP DATABASE INF354
+Go
+CREATE DATABASE INF354
+go
+
+USE INF354
+GO
+
+CREATE TABLE tblFurnitureType (
+  id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  Type varchar(255) NOT NULL
+) 
+
+CREATE TABLE tblTitle (
+  id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  Title varchar(3) NOT NULL
+) 
+
+CREATE TABLE tblClient (
+  id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  Name varchar(100) NOT NULL,
+  Lastname varchar(100) NOT NULL,
+  email varchar(255) NOT NULL UNIQUE,
+  titleID int NOT NULL FOREIGN KEY REFERENCES tblTitle(id)
+)
+
+CREATE TABLE tblFurniture (
+  id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  Name varchar(255) NOT NULL,
+  typeID int NOT NULL FOREIGN KEY REFERENCES tblFurnitureType(id)
+)
+
+CREATE TABLE tblClientFurniture (
+  clientId int NOT NULL FOREIGN KEY REFERENCES tblClient(id),
+  furnitureId int NOT NULL FOREIGN KEY REFERENCES tblFurniture(id),
+  date date NOT NULL
+)
+GO
+
+
+
